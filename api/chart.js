debdff59741dace6679e60a5b2ed5c42cf3f5865
xiaoming <852527299@qq.com>
2025-08-25 17:50:04 +0800

    Update chart.js

--- api/chart.js
+++ api/chart.js
@@ -1,37 +1,4 @@
-export default async (req, res) => {
-    const html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta charset="utf-8">
-        <title>简单 ECharts 示例</title>
-        <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
-        <style>
-          body { margin: 0; }
-          #main { width: 100vw; height: 100vh; }
-        </style>
-      </head>
-      <body>
-        <div id="main"></div>
-        <script>
-          var myChart = echarts.init(document.getElementById('main'));
-          var option = {
-            title: { text: 'ECharts 入门示例' },
-            tooltip: {},
-            xAxis: { data: ['A', 'B', 'C', 'D', 'E'] },
-            yAxis: {},
-            series: [{
-              name: '销量',
-              type: 'bar',
-              data: [5, 20, 36, 10, 10]
-            }]
-          };
-          myChart.setOption(option);
-        </script>
-      </body>
-      </html>
-    `;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(html);
-  };
-  
+export default (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Zeabur API!');
+};
